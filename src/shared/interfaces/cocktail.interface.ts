a4662e8884b77f52f7d1d14321c79f271717cd44
Matt Knickelbine <mknickelbine@gmail.com>
2021-05-10 19:02:34 -0500

    added data models

---
+++ src/shared/interfaces/cocktail.interface.ts
@@ -0,0 +1,34 @@
+export interface ICocktail {
+    id: string;
+    dateModified: string; // ISO DateTime
+    name: string;
+    nameAlternate?: string;
+    tags: string[];
+    videoUrl?: string;
+    category: string;
+    iba?: string;
+    isAlcoholic: boolean;
+    glassType?: string;
+    instructions: {
+        [key: string]: string;
+    };
+    imageUrl?: string;
+    imageSrc?: string;
+    imageAttribution?: string;
+    isCreativeCommonsConfirmed: boolean;
+    ingredients: IIngredientStub[];
+}
+
+export interface IIngredientStub {
+    name: string;
+    measurement: string;
+}
+
+export interface IIngredient {
+    id: string;
+    name: string;
+    description?: string;
+    type: string;
+    isAlcohol: boolean;
+    abv?: string;
+}
